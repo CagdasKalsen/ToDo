88803e0cff0930a75fd7a7aebc76dc49fbbf293f
cagdaskalsen <cagdaskalsen@hotmail.com>
2023-02-28 00:03:59 -0500

    .

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h2> Welcome to the Todo List App developed by Cagdas Kalsen.</h2>
+      <h2> Welcome to the " Todo List" developed by Cagdas Kalsen.</h2>
       <h2>
         Start by clicking on the "TODOS" button located on the top right of the
         page. This will take you to the main Todo List screen.
